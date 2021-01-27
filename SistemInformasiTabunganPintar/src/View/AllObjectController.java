c10dccee5c5fdc13f5cea80d049f5cb79b0a9499
SyahrulAndiSantoso <76801941+SyahrulAndiSantoso@users.noreply.github.com>
2021-01-27 21:31:04 +0700

    Add files via upload

---
+++ SistemInformasiTabunganPintar/src/View/AllObjectController.java
@@ -0,0 +1,8 @@
+package View;
+
+import Controller.PenggunaController;
+import Entity.PenggunaEntity;
+public class AllObjectController {
+    public static PenggunaController user = new PenggunaController();
+    public static PenggunaEntity pengguna = new PenggunaEntity();
+}
